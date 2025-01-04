cb1dca82a177b13e5180b1c9e2bf53d4eda1949a
sriramgorthi <23A91A6119@aec.edu.in>
2025-01-04 11:52:33 +0530

    Uploading code: Insert new Customer Record.sql

---
+++ Insert new Customer Record.sql
@@ -0,0 +1,5 @@
+INSERT INTO Customer (CustomerID, FullName, Email, City, RegistrationDate, Status)
+VALUES (5, 'John Doe', 'john.doe@example.com', 'New York', '2024-01-01', 'Active');
+
+SELECT * 
+FROM Customer;
